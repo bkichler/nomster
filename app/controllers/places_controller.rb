cdf794dcdbd0f80321521f91c946a8a83d0a4b20
Brian Kichler <brian.kichler@gmail.com>
2018-03-12 01:43:15 +0000

    Added edit logic

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :update]
 
   def index
     @places = Place.order(:name).page(params[:page]).per(5)
@@ -16,6 +16,16 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find(params[:id])
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
   end
 
   private
