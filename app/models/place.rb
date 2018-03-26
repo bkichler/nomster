32fed75da33eca168e92bc9771468c384e27a47d
Brian Kichler <brian.kichler@gmail.com>
2018-03-26 20:00:57 +0000

    Fixed syntax errors in view causing submit logic to break

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,10 +3,11 @@ class Place < ApplicationRecord
  belongs_to :user
  has_many :comments
  has_many :photos
- 
+
  geocoded_by :address
  after_validation :geocode
- 
+
  validates :name, presence: true, length: { minimum: 3, message: "name must be greater than 3 characters" }
- validates :description, :address, presence: true
+ validates :description, presence: true
+ validates :address, presence: true
 end
